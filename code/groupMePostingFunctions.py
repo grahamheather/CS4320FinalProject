8955f26d49e3ebb9ec66d1b1598745fd21d9e4fa
Rap <hmgxdd@mail.missouri.edu>
2017-11-05 12:04:52 -0600

    Implement posting sprint start, end, and length

--- code/groupMePostingFunctions.py
+++ code/groupMePostingFunctions.py
@@ -1,9 +1,31 @@
 import urllib2
 import json
+import time
+from datetime import datetime
 from operator import itemgetter
 from config import bot_id
 from config import githubRepo
 from config import waffle
+
+# helper functions
+def getCurrentMilestone():
+    response = urllib2.urlopen("https://api.github.com/repos/"+githubRepo+"/milestones")
+    result = response.read()
+    decoder = json.JSONDecoder()
+    data = decoder.decode(result)
+    earliest = None
+    earliest_milestone = None
+    for milestone in data:
+        if earliest == None:
+            earliest = milestone['due_on']
+            earliest_milestone = milestone
+        # may want to check if the earliest sprint is actually unclosed but already completed sprint
+        elif time.strptime(milestone['due_on'], "%Y-%m-%dT%H:%M:%SZ") < time.strptime(earliest, "%Y-%m-%dT%H:%M:%SZ"):
+            earliest = milestone['due_on']
+            earliest_milestone = milestone
+    return earliest_milestone
+
+# posting functions
 
 def postOpenIssues():
     response = urllib2.urlopen("https://api.github.com/repos/"+githubRepo+"/issues")
@@ -63,33 +85,36 @@ def postInProgressCards():
 #def assignPointsToStory():
 
 def postSprintGoal():
-    response = urllib2.urlopen("https://api.github.com/repos/"+githubRepo+"/milestones")
-    result = response.read()
-    decoder = json.JSONDecoder()
-    data = decoder.decode(result)
-    earliest = None
-    earliest_milestone = None
-    for milestone in data:
-        if earliest == None:
-            earliest = milestone['due_on']
-            earliest_milestone = milestone
-        # may want to check if the earliest sprint is actually unclosed but already completed sprint
-        elif time.strptime(milestone['due_on'], "%Y-%m-%dT%H:%M:%SZ") < time.strptime(earliest, "%Y-%m-%dT%H:%M:%SZ"):
-            earliest = milestone['due_on']
-            earliest_milestone = milestone
-    if earliest_milestone == None:
+    current_milestone = getCurrentMilestone()
+    if current_milestone == None:
         string = "Cannot print current sprint goal: no current sprint"
     else:
-        string = "The current sprint goal is:\\n" + milestone['description'].replace('\r', '').replace('\n', '\\n')
-    print "https://api.groupme.com/v3/bots/post",  '{"text" : "' + string + '", "bot_id" : "' + bot_id + '"}'
+        string = "The current sprint goal is:\\n" + current_milestone['description'].replace('\r', '').replace('\n', '\\n')
     response = urllib2.urlopen("https://api.groupme.com/v3/bots/post",  '{"text" : "' + string + '", "bot_id" : "' + bot_id + '"}')
-            
 
-#def postSprintLength():
+def postSprintLength():
+    current_milestone = getCurrentMilestone()
+    if current_milestone == None:
+        string = "Cannot print current sprint length: no current sprint"
+    else:
+        string = "The current sprint is " + str((datetime.strptime(current_milestone['due_on'], "%Y-%m-%dT%H:%M:%SZ") - datetime.strptime(current_milestone['created_at'], "%Y-%m-%dT%H:%M:%SZ")).days + 1) + " day(s) long."
+    response = urllib2.urlopen("https://api.groupme.com/v3/bots/post",  '{"text" : "' + string + '", "bot_id" : "' + bot_id + '"}')
 
-#def postSprintStart():
+def postSprintStart():
+    current_milestone = getCurrentMilestone()
+    if current_milestone == None:
+        string = "Cannot print current sprint start: no current sprint"
+    else:
+        string = "The current sprint began " + str((datetime.now() - datetime.strptime(current_milestone['created_at'], "%Y-%m-%dT%H:%M:%SZ")).days + 1) + " day(s) ago at " + current_milestone['created_at']
+    response = urllib2.urlopen("https://api.groupme.com/v3/bots/post",  '{"text" : "' + string + '", "bot_id" : "' + bot_id + '"}')
 
-#def postSprintEnd():
+def postSprintEnd():
+    current_milestone = getCurrentMilestone()
+    if current_milestone == None:
+        string = "Cannot print current sprint end: no current sprint"
+    else:
+        string = "The current sprint ends in " + str((datetime.strptime(current_milestone['due_on'], "%Y-%m-%dT%H:%M:%SZ") - datetime.now()).days + 1) + " day(s) at " + current_milestone['due_on']
+    response = urllib2.urlopen("https://api.groupme.com/v3/bots/post",  '{"text" : "' + string + '", "bot_id" : "' + bot_id + '"}')
 
 #def moveCard():
 
@@ -114,5 +139,3 @@ def postSprintGoal():
 #def startSprintReview():
 
 #def startSprintRetrospective():
-
-postSprintGoal()
